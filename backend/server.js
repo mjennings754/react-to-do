9183a3000975e0aa4d16f5dc05e5bf6f607df45e
mjennings754 <mitchellj@outlook.jp>
2026-03-17 05:48:43 +1100

    add routing:

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,20 @@ const app = express()
 
 app.get("/api/tasks", (_, res) => {
     res.send("You got 5 tasks")
-})
+});
+
+app.post("api/tasks", (_, res) => {
+    res.status(201).json({message: "Task created successfully!"})
+});
+
+app.put("api/tasks/:id", (_, res) => {
+    res.status(200).json({message: "Task updated successfully!"})
+});
+
+app.delete("api/tasks/:id", (_, res) => {
+    res.status(200).json({message: "Task deleted successfully!"})
+});
 
 app.listen(5001, () => {
     console.log("Server is running")
-})
+});
