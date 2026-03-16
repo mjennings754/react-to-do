af7f688d22664b3e30c6e2d2179f4769e2f88a29
mjennings754 <mitchellj@outlook.jp>
2026-03-17 07:43:21 +1100

    add mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import tasksRoutes from "./routes/tasksRoutes.js"
-const app = express()
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001
+
+connectDB();
 
 app.use("/api/tasks", tasksRoutes)
 
-app.listen(5001, () => {
-    console.log("Server is running")
+app.listen(PORT, () => {
+    console.log("Server is running on: ", PORT)
 });
+
